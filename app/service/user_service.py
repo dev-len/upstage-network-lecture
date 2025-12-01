3309e054022fc6704678b1fa4509bdedf4886892
inspire12 <ox4443@naver.com>
2025-12-01 14:02:07 +0900

    feat: memory repo 추가

--- app/service/user_service.py
+++ app/service/user_service.py
@@ -1,10 +1,12 @@
 from typing import Dict, Any
 from app.exceptions import EmailNotAllowedNameExistsError
 
+from app.repository.user_repo import UserRepository
+
 
 class UserService:
-    def __init__(self):
-        pass
+    def __init__(self, user_repo: UserRepository):
+        self.user_repo = user_repo
 
     def _valid_email(self, email: str) -> bool:
         return True
@@ -15,4 +17,13 @@ class UserService:
         if email == "admin@example.com":
             raise EmailNotAllowedNameExistsError(email)
         # save 추가
-        return {'id': 1, 'name': name, 'email': email}
+        user = self.user_repo.save(name=name, email=email)
+        return  {'id': user.id, 'name': user.name,
+                'email': user.email, 'created_at': str(user.created_at)}
+
+
+    def get_user(self, user_id: int) -> Dict[str, Any]:
+
+        user = self.user_repo.find_by_id(user_id=user_id)
+        return {'id': user.id, 'name': user.name,
+                'email': user.email, 'created_at': str(user.created_at)}
